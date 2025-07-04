f221499ff7d6f660e4f48d619f12da9f8703c907
robii <robii@192.168.1.200>
2025-07-04 16:47:52 +0300

    test

---
+++ src/main/java/com/mycompany/reprezentantavolvo/ReprezentantaVolvo.java
@@ -0,0 +1,25 @@
+package com.mycompany.reprezentantavolvo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ReprezentantaVolvo {
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Volvo", "postgres", "password")){
+            System.out.println("connection.isValid(0) = " + connection.isValid(0));
+            
+            ClientiDAO clientiDAO = new ClientiDAO(connection);
+            List<Clienti> clientiGasiti = clientiDAO.cautaClientiDupaNume("Robert");
+            
+            for (Clienti c : clientiGasiti){
+                System.out.println(c.getCODC() + " - "+c.getNume() + " - "+c.getLocalitate());
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
