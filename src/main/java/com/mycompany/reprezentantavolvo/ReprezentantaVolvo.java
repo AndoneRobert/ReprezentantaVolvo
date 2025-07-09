d8ec92066061aa1bd15c7cee0a5047dfb916fecc
robii <robii@192.168.1.200>
2025-07-09 16:54:19 +0300

    testare insert ClientiDAO

--- src/main/java/com/mycompany/reprezentantavolvo/ReprezentantaVolvo.java
+++ src/main/java/com/mycompany/reprezentantavolvo/ReprezentantaVolvo.java
@@ -10,8 +10,13 @@ public class ReprezentantaVolvo {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Volvo", "postgres", "password")){
             System.out.println("connection.isValid(0) = " + connection.isValid(0));  
+            
+        ClientiDAO dao = new ClientiDAO(connection);
+        dao.insertClient("TESTNUME", "TESTLOC");
         } catch (SQLException e){
             e.printStackTrace();
         }
+        
+
     }
 }
