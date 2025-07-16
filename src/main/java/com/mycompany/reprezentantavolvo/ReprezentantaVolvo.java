49117e04643f6edd41057c6b4cd2e5d4bb3d8fbc
robii <robii@192.168.1.200>
2025-07-16 14:12:16 +0300

    new db connection

--- src/main/java/com/mycompany/reprezentantavolvo/ReprezentantaVolvo.java
+++ src/main/java/com/mycompany/reprezentantavolvo/ReprezentantaVolvo.java
@@ -3,20 +3,22 @@ package com.mycompany.reprezentantavolvo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 
 public class ReprezentantaVolvo {
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Volvo", "postgres", "password")){
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:5432/postgres",
+                "postgres.zejkokxtnrzexlrdgeyp",
+                "hHe2D2jEPRXWrRVR")) {
+
             System.out.println("connection.isValid(0) = " + connection.isValid(0));  
-            
-        ClientiDAO dao = new ClientiDAO(connection);
-        dao.insertClient("TESTNUME", "TESTLOC");
-        } catch (SQLException e){
+
+            ClientiDAO dao = new ClientiDAO(connection);
+            dao.insertClient("TESTNUME", "TESTLOC");
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-
     }
 }
