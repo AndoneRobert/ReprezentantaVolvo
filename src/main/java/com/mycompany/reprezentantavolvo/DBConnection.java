48c8214bce9bbbf30132cdcb3eb4b64c61b18877
robii <robii@192.168.1.200>
2025-07-16 14:12:12 +0300

    new db connection

--- src/main/java/com/mycompany/reprezentantavolvo/DBConnection.java
+++ src/main/java/com/mycompany/reprezentantavolvo/DBConnection.java
@@ -1,19 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.reprezentantavolvo;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-/**
- *
- * @author robii
- */
+
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/Volvo";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:5432/postgres";
+    private static final String USER = "postgres.zejkokxtnrzexlrdgeyp";
+    private static final String PASSWORD = "hHe2D2jEPRXWrRVR";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
